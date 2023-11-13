019c61a631b9c15a85f344a453650741e5ebf2f0
Woo Zhong Han <hanwj100@gmail.com>
2023-11-12 21:55:30 -0500

    Update UI for CustomerAddressScreen

--- customer-app/screens/CustomerAddressScreen.tsx
+++ customer-app/screens/CustomerAddressScreen.tsx
@@ -1,66 +1,119 @@
-import React, { useState } from 'react';
-import { View, Text, TextInput, Button, SafeAreaView } from 'react-native';
-import { useUser } from "../UserContext";
+import { ParamListBase, useNavigation } from "@react-navigation/native";
 import { StackNavigationProp } from "@react-navigation/stack";
-import { useNavigation, ParamListBase } from "@react-navigation/native";
-import { StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView, ScrollView, StyleSheet, Switch, Text, TextInput, View } from 'react-native';
+import { Border, Color, FontFamily, FontSize, Padding } from '../GlobalStyles';
+import { useUser } from "../UserContext";
+import BackButton from "../components/BackButton";
+import ContinueButton from "../components/ContinueButton";
+import PageHeader from "../components/PageHeader";
 
 const CustomerAddressScreen: React.FC<{}> = () => {
   const navigation = useNavigation<StackNavigationProp<ParamListBase>>();
   const { user } = useUser();
   const [address, setAddress] = useState<string>('');
+  const [useCurrentAddress, setUseCurrentAddress] = useState<boolean>(true);
 
-  const handleUseCurrentAddress = () => {
-    const address = user.address;
-    navigation.navigate('CustomerPickupDateTime', { address });
-  };
-
-  const handleUseNewAddress = () => {
-    navigation.navigate('CustomerPickupDateTime', { address });
+  const handleConfirmAddress = () => {
+    const selectedAddress = useCurrentAddress ? user.address : address;
+    navigation.navigate('CustomerPickupDateTime', { address: selectedAddress });
   };
 
   return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>Choose Address for Order</Text>
-      <Text style={styles.currentAddress}>Current Address: {user.address}</Text>
-      <Button title="Use Current Address" onPress={handleUseCurrentAddress} />
-      <Text style={styles.subtitle}>Or enter a new address:</Text>
-      <TextInput
-        style={styles.input}
-        value={address}
-        onChangeText={setAddress}
-        placeholder="Enter new address"
-      />
-      <Button title="Use New Address" onPress={handleUseNewAddress} />
+    <SafeAreaView style={styles.safeArea}>
+      <ScrollView style={styles.scrollView}>
+        <View style={styles.container}>
+          <BackButton />
+
+          <PageHeader
+            title="Choose Address for Order"
+            subtitle="Select your current address or enter a new one."
+          />
+
+          <View style={styles.switchContainer}>
+            <Text style={styles.switchLabel}>Use Current Address</Text>
+            <Switch
+              trackColor={{ false: "#767577", true: Color.color }}
+              thumbColor={useCurrentAddress ? "#f4f3f4" : "#f4f3f4"}
+              ios_backgroundColor="#3e3e3e"
+              onValueChange={() => setUseCurrentAddress(previousState => !previousState)}
+              value={useCurrentAddress}
+            />
+          </View>
+
+          {useCurrentAddress ? (
+            <Text style={styles.currentAddress}>Current Address: {user.address}</Text>
+          ) : (
+            <>
+              <Text style={styles.label}>Enter New Address:</Text>
+              <TextInput
+                style={styles.input}
+                value={address}
+                onChangeText={setAddress}
+                placeholder="Enter new address"
+              />
+            </>
+          )}
+        </View>
+      </ScrollView>
+
+      <View style={styles.bottomContainer}>
+        <ContinueButton
+          destination="CustomerPickupDateTime"
+          buttonText="Continue"
+          onPressAdditional={handleConfirmAddress}
+        />
+      </View>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
+  safeArea: {
     flex: 1,
-    padding: 20,
-    backgroundColor: '#fff',
+    backgroundColor: Color.colorWhite,
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    marginBottom: 10,
+  scrollView: {
+    flex: 1,
+  },
+  container: {
+    padding: Padding.p_11xl,
+  },
+  switchContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginBottom: 20,
+  },
+  switchLabel: {
+    fontSize: FontSize.size_base,
+    color: Color.color1,
+    fontFamily: FontFamily.montserratMedium,
   },
   currentAddress: {
-    fontSize: 16,
-    marginBottom: 10,
+    fontSize: FontSize.size_base,
+    marginBottom: 20,
+    color: Color.color1,
+    fontFamily: FontFamily.montserratMedium,
   },
-  subtitle: {
-    fontSize: 16,
-    marginTop: 20,
+  label: {
+    fontSize: FontSize.size_base,
     marginBottom: 10,
+    color: Color.color1,
+    fontFamily: FontFamily.montserratMedium,
   },
   input: {
-    height: 40,
-    borderColor: 'gray',
-    borderWidth: 1,
-    paddingLeft: 10,
+    borderWidth: 2,
+    borderColor: Color.color,
+    borderRadius: Border.br_6xl,
+    padding: 20,
+    marginBottom: 20,
+    fontSize: FontSize.size_base,
+    color: Color.color1,
+    fontFamily: FontFamily.montserratMedium,
+  },
+  bottomContainer: {
+    padding: Padding.p_11xl,
   },
 });
 
