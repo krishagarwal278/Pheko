9ff3c20f61c21fff7a521ab7ffc8090fedf3bf61
Woo Zhong Han <hanwj100@gmail.com>
2023-11-13 14:20:24 -0500

    Improve UI for CustomerOTPVerification page

--- customer-app/screens/CustomerOTPVerification.tsx
+++ customer-app/screens/CustomerOTPVerification.tsx
@@ -1,24 +1,22 @@
-import * as React from "react";
+import { RouteProp, useNavigation, useRoute } from '@react-navigation/native';
+import { StackNavigationProp } from '@react-navigation/stack';
+import { PhoneAuthProvider, signInWithCredential } from 'firebase/auth';
+import { doc, getDoc, getFirestore } from 'firebase/firestore';
+import React, { useState } from 'react';
 import {
-  View,
-  TextInput,
-  Text,
-  Image,
-  StyleSheet,
-  TouchableOpacity,
+  KeyboardAvoidingView,
+  Platform,
   SafeAreaView,
-  Dimensions,
-} from "react-native";
-import { Color } from "../GlobalStyles";
-import { useNavigation, ParamListBase } from "@react-navigation/native";
-import { StackNavigationProp } from "@react-navigation/stack";
-import { useRoute, RouteProp } from "@react-navigation/native";
-import { getFirestore, doc, getDoc } from "firebase/firestore";
-import { PhoneAuthProvider, signInWithCredential } from "firebase/auth";
-import { app, auth } from "../Firebase";
-import { useState } from "react";
-import { useUser } from "../UserContext";
-import {User} from "../Types";
+  StyleSheet,
+  TextInput,
+  View
+} from 'react-native';
+import { app, auth } from '../Firebase';
+import { Border, Color, FontFamily, FontSize, Padding } from '../GlobalStyles';
+import { useUser } from '../UserContext';
+import BackButton from '../components/BackButton';
+import ContinueButton from '../components/ContinueButton';
+import PageHeader from '../components/PageHeader'; // Added for UI consistency
 
 type VerificationRouteParams = {
   CustomerOTPVerification: {
@@ -26,84 +24,61 @@ type VerificationRouteParams = {
   };
 };
 
-const { width, height } = Dimensions.get("window");
-
 const CustomerOTPVerification: React.FunctionComponent = () => {
-  const navigation = useNavigation<StackNavigationProp<ParamListBase>>();
-  const route =
-    useRoute<RouteProp<VerificationRouteParams, "CustomerOTPVerification">>();
+  const navigation = useNavigation<StackNavigationProp<any>>();
+  const route = useRoute<RouteProp<VerificationRouteParams, 'CustomerOTPVerification'>>();
   const { verificationId } = route.params;
-
-  const [code, setCode] = useState("");
-
-  const { user, setUser } = useUser();
+  const [code, setCode] = useState('');
+  const { setUser } = useUser();
 
   const confirmCode = async () => {
-    const credential = PhoneAuthProvider.credential(verificationId, code);
     try {
+      const credential = PhoneAuthProvider.credential(verificationId, code);
       const result = await signInWithCredential(auth, credential);
-      const user = result.user;
-
-      // Check if the user exists in Firestore
       const db = getFirestore(app);
-      const userDocRef = doc(db, "Users", user.uid);
+      const userDocRef = doc(db, 'Users', result.user.uid);
       const userDoc = await getDoc(userDocRef);
 
       if (userDoc.exists()) {
-        console.log("User already registered. Signed in successfully.");
         const userData = userDoc.data();
-        const document: User = {
-          id: userDoc.id,
-          address: userData.address,
-          dateOfBirth: userData.dateOfBirth,
-          firstName: userData.firstName,
-          lastName: userData.lastName,
-          email: userData.email,
-          phone: userData.phone,
-          dateCreated: userData.dateCreated,
-          dateLastUpdated: userData.dateLastUpdated
-        }
-        setUser(document);
-        navigation.navigate("CustomerDashboard");
+        setUser({ ...userData, id: userDoc.id });
+        navigation.navigate('CustomerDashboard');
       } else {
-        console.log("User not registered. Redirecting to sign up form.");
-        navigation.navigate("CustomerSignUp");
+        navigation.navigate('CustomerSignUp');
       }
     } catch (error) {
-      console.log("Error during sign-in:", error);
+      console.error('Error during OTP verification:', error);
     }
   };
 
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: Color.colorWhite }}>
-      <View style={styles.container}>
-        <View style={styles.vectorContainer}>
-          <Image
-            style={styles.vectorImage}
-            resizeMode="contain"
-            source={require("../assets/vector1.png")}
+      <KeyboardAvoidingView 
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.container}
+      >
+        <BackButton />
+
+        <PageHeader // Using PageHeader for consistency
+          title="Verify Your Number"
+          subtitle="Enter the OTP sent to your number"
+        />
+
+        <TextInput
+          style={styles.input}
+          placeholder="Enter OTP"
+          onChangeText={setCode}
+          keyboardType="number-pad"
+        />
+
+        <View style={styles.bottomContainer}>
+          <ContinueButton
+            destination="CustomerDashboard" // Modify as needed
+            buttonText="Confirm OTP"
+            onPressAdditional={confirmCode}
           />
         </View>
-
-        <Text style={styles.header}>What's the OTP?</Text>
-
-        <View style={styles.form}>
-          <Text style={styles.label}>
-            Enter the OTP sent to your mobile number
-          </Text>
-
-          <TextInput
-            style={styles.input}
-            placeholder="Enter your OTP"
-            onChangeText={setCode}
-            keyboardType="number-pad"
-            autoComplete="tel"
-          />
-          <TouchableOpacity style={styles.submitButton} onPress={confirmCode}>
-            <Text style={styles.submitButtonText}>Confirm verification</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
+      </KeyboardAvoidingView>
     </SafeAreaView>
   );
 };
@@ -111,53 +86,22 @@ const CustomerOTPVerification: React.FunctionComponent = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: width * 0.05,
-    backgroundColor: "#fff",
-    justifyContent: "flex-start",
-  },
-  vectorContainer: {
-    width: width * 0.05,
-    height: height * 0.025,
-    marginBottom: height * 0.02,
-    alignSelf: "flex-end",
-  },
-  vectorImage: {
-    flex: 1,
-    width: undefined,
-    height: undefined,
-  },
-  header: {
-    fontSize: width * 0.05,
-    fontWeight: "bold",
-    marginBottom: height * 0.02,
-  },
-  form: {
-    flex: 1,
-  },
-  label: {
-    fontWeight: "500",
-    marginBottom: height * 0.005,
-    fontSize: width * 0.04,
+    padding: Padding.p_11xl,
   },
   input: {
     borderWidth: 2,
-    borderColor: "#0f5b28",
-    borderRadius: 25,
-    padding: width * 0.02,
-    marginBottom: height * 0.02,
-    fontSize: width * 0.04,
+    borderColor: Color.color,
+    borderRadius: Border.br_6xl,
+    padding: 20,
+    marginBottom: 20,
+    fontSize: FontSize.size_base,
+    color: Color.color1,
+    fontFamily: FontFamily.montserratMedium,
   },
-  submitButton: {
-    backgroundColor: "#bffa01",
-    padding: height * 0.02,
-    borderRadius: 25,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  submitButtonText: {
-    color: "#fff",
-    fontSize: width * 0.05,
-    fontWeight: "bold",
+  bottomContainer: {
+    flex: 1,
+    justifyContent: "flex-end",
+    marginBottom: 20,
   },
 });
 
